8df4f8e909f3051c4f9d88014766def43f4ddc46
Daniel Atonge <elamboatonge@gmail.com>
2019-09-26 23:04:48 +0300

    catching signals and providing feedback

--- week06/ex3.c
+++ week06/ex3.c
@@ -1,7 +1,26 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<signal.h>
+#include<stdlib.h>
 
-int main() {
-    
-    return 0;
+void signal_handler(int);
+
+int main(void)
+{
+   signal(SIGINT, signal_handler);
+   while (1)
+        sleep(1);
+   return 0;
 }
 
+void  signal_handler(int sig)
+{
+   char  c;
+   signal(sig, SIG_IGN);
+   printf("\nHey, You pressed Ctrl+C. Do you really want to quit? [y/n] ");
+   c = getchar();
+   if (c == 'y' || c == 'Y')
+        exit(0);
+   else
+        signal(SIGINT, signal_handler);
+   getchar();
+}
