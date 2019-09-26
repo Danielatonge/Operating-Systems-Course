4ff43178b479b7b233b7dbfc29c9407a8d30bff8
Daniel Atonge <elamboatonge@gmail.com>
2019-09-26 23:54:27 +0300

    killing child processes

--- week06/ex5.c
+++ week06/ex5.c
@@ -1,7 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<signal.h>
+#include<unistd.h>
 
-#include <stdio.h>
+void hello(){
+  signal(SIGTERM, hello);
+  printf("Killing child process\n");
+}
 
-int main() {
-    
-    return 0;
+int main(){
+
+  pid_t cpid;
+  pid_t ppid;
+
+  //set handler for SIGTERM to hello()
+  signal(SIGTERM, hello);
+  cpid = fork();
+
+  if (cpid == 0){
+
+    while(1) {
+      printf("I'm alive\n");
+      sleep(1);
+    }
+
+  } else {
+    /*PARENT*/
+    printf("cpid %d\n", cpid);
+    sleep(10);
+    //just wait for child to terminate
+
+    //send SIGTERM signal to CHILD
+    kill(cpid, SIGTERM);
+    exit(0);
+  }
+
 }
