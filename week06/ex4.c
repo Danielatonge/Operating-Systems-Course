b87a41c269b7f7545857fa3c8647989b6f0e77df
Daniel Atonge <elamboatonge@gmail.com>
2019-09-26 23:24:14 +0300

    handling user defined and uncatchable signals

--- week06/ex4.c
+++ week06/ex4.c
@@ -1,7 +1,30 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<signal.h>
+#include<unistd.h>
 
-int main() {
-    
-    return 0;
+void signal_handler(int signo)
+{
+    if (signo == SIGUSR1)
+        printf("received SIGUSR1\n");
+    else if (signo == SIGKILL)
+        printf("received SIGKILL\n");
+    else if (signo == SIGSTOP)
+        printf("received SIGSTOP\n");
 }
 
+int main()
+{
+    // Print pid, so that we can send signals from other shells
+    printf("My pid is: %d\n", getpid());
+
+    if (signal(SIGUSR1, signal_handler) == SIG_ERR)
+        printf("\ncan't catch SIGUSR1\n");
+    if (signal(SIGKILL, signal_handler) == SIG_ERR)
+        printf("\ncan't catch SIGKILL\n");
+    if (signal(SIGSTOP, signal_handler) == SIG_ERR)
+        printf("\ncan't catch SIGSTOP\n");
+    // A long long wait so that we can easily issue a signal to this process
+    while(1)
+        sleep(1);
+    return 0;
+}
