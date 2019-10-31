bed58e1c95ea2f1fea76b3d41c4cf94e99e0891a
Hardriive <elamboatonge@gmail.com>
2019-10-31 21:07:34 +0300

    exercise 2 updated sleep header

---
+++ week11/ex2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+
+  printf("H");
+  sleep(1);
+  printf("E");
+  sleep(1);
+  printf("L");
+  sleep(1);
+  printf("L");
+  sleep(1);
+  printf("O");
+  sleep(1);
+  printf("\n");
+  return 0;
+}
