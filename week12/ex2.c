3304db258839c66a97d59ad5aa12f1201a3fbb51
Hardriive <elamboatonge@gmail.com>
2019-11-07 23:29:43 +0300

    update to input output

--- week12/ex2.c
+++ week12/ex2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <getopt.h>
 
 static int append_mode = 0;
@@ -12,15 +13,8 @@ int parse_args(int argc, char *argv[])
 		case 'a':
 			append_mode = 1;
 			break;
-		case '?':
-			if (isprint(optopt))
-				fprintf(stderr, "Unkonw option `-%c'.\n", optopt);
-			else
-				fprintf(stderr,
-					"Unknown option character `\\x%x'.\n", optopt);
-			return 1;
 		default:
-			abort();
+      return 1;
 			break;
 		}
 	}
@@ -32,8 +26,8 @@ int main(int argc, char *argv[])
 	char buffer[100];
 	size_t len;
 	char *file_mode;
-    int i;
-	FILE *files[20];
+  int i;
+	FILE *files[10];
 	int num_files;
 
 	if (parse_args(argc, argv)) {
@@ -44,26 +38,15 @@ int main(int argc, char *argv[])
 
 	num_files = argc - optind;
 	if (num_files > 0) {
-		if (files == NULL) {
-			fprintf(stderr, "Unable to allocate file buffer space\n");
-			return 1;
-		}
-
 		/* go through file arguments and either open for writing
 		   or append based on the -a flag */
 		for (i = optind; i < argc; i++) {
 			FILE *pFile = fopen(argv[i], file_mode);
-			if (pFile == NULL)
-			{
-				fprintf(stderr, "Unable to open file %s for mode %s",
-					argv[i], file_mode);
-				goto main_cleanup;
-			}
 			files[i - optind] = pFile;
 		}
 	}
 
-	FILE *not_stdin = fopen("tee.c", "r");
+	FILE *not_stdin = fopen("ex1.c", "r");
 	while ((len = fread(&buffer[0], 1, sizeof(buffer), not_stdin)) > 0) {
 		fwrite(&buffer[0], 1, len, stdout);
 		for (i = 0; i < num_files; i++) {
@@ -71,7 +54,6 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	main_cleanup:
 	if (num_files > 0) {
 		for (i = 0; i < num_files; i++) {
 			fclose(files[i]);
